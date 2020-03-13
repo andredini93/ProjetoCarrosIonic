b79d67490e0eabb39984cdf7d1380ed9c1c39215
andredini93 <andredini93@gmail.com>
2020-03-13 10:02:24 -0300

    Aula 04

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -17,12 +17,27 @@ import { Carro } from '../../Modelos/Carro';
 export class EscolhaPage {
 
   public carro: Carro;
+  public acessorios = [];
+  private _precoTotal: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = this.navParams.get('carroSelecionado');
+    this._precoTotal = this.carro.preco;
+    this.acessorios = [
+      {nome: 'Freio ABS', preco: 800},
+      {nome: 'Ar-condicionado', preco: 1000},
+      {nome: 'MP3 Player', preco: 500}
+    ]
   }
 
-  ionViewDidLoad() {
+  atualizaTotal(ativado:boolean, precoAcessorio){
+    ativado ?
+      this._precoTotal += precoAcessorio :
+      this._precoTotal -= precoAcessorio;
+  }
+
+  get precoTotal() {
+    return this._precoTotal;
   }
 
 }
